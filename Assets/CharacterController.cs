50ef146e2544211f42952abb8da98f3f5f75b161
Natasha Natasha Iqbal <niqbal2021@elam.co.uk>
2021-11-16 14:49:57 +0000

    added movng platfrom and jump

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -9,6 +9,12 @@ public class CharacterController : MonoBehaviour
     float camRotation = 0.0f;
     GameObject cam;
     Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
     public float rotationSpeed = 2.0f;
     public float camRotationSpeed = 1.5f;
 
@@ -23,6 +29,12 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis ("Horizontal") * maxSpeed);
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
